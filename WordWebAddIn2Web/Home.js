7b342f9c20cae110b8b1e6548ffba0f0aed3c81c
Kunwar Vikrant Singh <kvsingh@quark.com>
2020-05-09 22:52:17 +0530

    Add project files.

---
+++ WordWebAddIn2Web/Home.js
@@ -0,0 +1,280 @@
+﻿
+(function () {
+    "use strict";
+
+    var messageBanner;
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Initialize the FabricUI notification mechanism and hide it
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+
+
+            Office.DocumentSelectionChanged
+            // If not using Word 2016, use fallback logic.
+            if (!Office.context.requirements.isSetSupported('WordApi', '1.1')) {
+                $("#template-description").text("This sample displays the selected text.");
+                $('#button-text').text("Display!");
+                $('#button-desc').text("Display the selected text");
+                
+                $('#highlight-button').click(displaySelectedText);
+
+                /*by Kunwar */                   
+                $('#SignIn').click(AuthenticateAndDisplay);
+                Office.DocumentSelectionChanged = function () {
+
+                    if (document.getElementsByName(abc123).selectedRow.innerHTML != Office.body.text)
+                    {
+                        window.alert("Validation failed");
+                    }
+
+                }
+               
+
+
+                return;
+            }
+
+            $("#template-description").text("This sample highlights the longest word in the text you have selected in the document.");
+            $('#button-text').text("Highlight!");
+            $('#button-desc').text("Highlights the longest word.");
+            
+            //loadSampleData();
+
+            // Add a click event handler for the highlight button.
+            $('#highlight-button').click(hightlightLongestWord);
+        });
+    };
+
+    function loadSampleData(textValue) {
+        // Run a batch operation against the Word object model.
+        Word.run(function (context) {
+            // Create a proxy object for the document body.
+            var body = context.document.body;
+
+            // Queue a commmand to clear the contents of the body.
+            body.clear();
+            // Queue a command to insert text into the end of the Word document body.
+            body.insertText(
+                textValue,
+                Word.InsertLocation.end);
+
+            // Synchronize the document state by executing the queued commands, and return a promise to indicate task completion.
+            return context.sync();
+        })
+        .catch(errorHandler);
+    }
+
+
+
+    //Kunwar
+    function AuthenticateAndDisplay()
+    {
+        var returnValue = false;
+        const userAction = async () => {
+            const response = await fetch('https://reqres.in/api/login');
+            const myJson = await response.json(); //extract JSON from the http response
+            response.forEach(function (object) {
+                //check against enterd values
+                //if match
+                returnValue = true;
+                
+            });
+
+            return false;
+        }
+        if (returnvalue) {
+
+            const userAction = async () => {
+                const response = await fetch('https://reqres.in/api/users?page=2');
+                const myJson = await response.json(); //extract JSON from the http response
+                response.forEach(function (object) {
+                    //check against enterd values
+                    //if match
+                    returnValue = true;
+
+                });
+
+                generateDynamicTable(response);
+            }
+        }
+        else
+        {
+                loadSampleData("Authentication failed!");
+        }
+    }
+
+    function generateDynamicTable(jsondata) {
+
+        var noOfContacts = jsondata.length;
+
+        if (noOfContacts > 0) {
+
+
+            // CREATE DYNAMIC TABLE.
+            var table = document.createElement("table");
+            table.id="abc123"
+            table.style.width = '50%';
+            table.setAttribute('border', '1');
+            table.setAttribute('cellspacing', '0');
+            table.setAttribute('cellpadding', '5');
+
+          
+
+            var col = []; // define an empty array
+            for (var i = 0; i < noOfContacts; i++) {
+                for (var key in myContacts[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
+
+            // CREATE TABLE HEAD .
+            var tHead = document.createElement("thead");
+
+
+            // CREATE ROW FOR TABLE HEAD .
+            var hRow = document.createElement("tr");
+
+            // ADD COLUMN HEADER TO ROW OF TABLE HEAD.
+            for (var i = 0; i < col.length; i++) {
+                var th = document.createElement("th");
+                th.innerHTML = col[i];
+                hRow.appendChild(th);
+            }
+            tHead.appendChild(hRow);
+            table.appendChild(tHead);
+
+            // CREATE TABLE BODY .
+            var tBody = document.createElement("tbody");
+
+            // ADD COLUMN HEADER TO ROW OF TABLE HEAD.
+            for (var i = 0; i < noOfContacts; i++) {
+
+                var bRow = document.createElement("tr"); // CREATE ROW FOR EACH RECORD .
+
+                for (var j = 0; j < col.length; j++) {
+                    var td = document.createElement("td");
+                    td.innerHTML = myContacts[i][col[j]];
+                    bRow.appendChild(td);
+                }
+                tBody.appendChild(bRow)
+
+            }
+            table.appendChild(tBody);
+
+            //Added Row click handler
+            var rows = table.getElementsByTagName("tr");
+            for (i = 0; i < rows.length; i++) {
+                var currentRow = table.rows[i];
+                var createClickHandler = function (row) {
+                    return function () {
+                        PutContentToWordCanas(currentRow);
+                    };
+                };
+                currentRow.onclick = createClickHandler(currentRow);
+            }
+
+
+
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("jsondata");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+
+        }
+    }
+
+    function PutContentToWordCanas(selectedRow)
+    {
+        var rowContent = selectedRow.innerHTML;
+
+        Word.run(function (context) {
+            // Create a proxy object for the document body.
+            var body = context.document.body;
+
+            // Queue a commmand to clear the contents of the body.
+            body.clear();
+            // Queue a command to insert text into the end of the Word document body.
+            body.insertText(
+                rowContent,
+                Word.InsertLocation.end);
+
+            // Synchronize the document state by executing the queued commands, and return a promise to indicate task completion.
+            return context.sync();
+        })
+            .catch(errorHandler);
+    }
+
+
+    function hightlightLongestWord() {
+        Word.run(function (context) {
+            // Queue a command to get the current selection and then
+            // create a proxy range object with the results.
+            var range = context.document.getSelection();
+            
+            // This variable will keep the search results for the longest word.
+            var searchResults;
+            
+            // Queue a command to load the range selection result.
+            context.load(range, 'text');
+
+            // Synchronize the document state by executing the queued commands
+            // and return a promise to indicate task completion.
+            return context.sync()
+                .then(function () {
+                    // Get the longest word from the selection.
+                    var words = range.text.split(/\s+/);
+                    var longestWord = words.reduce(function (word1, word2) { return word1.length > word2.length ? word1 : word2; });
+
+                    // Queue a search command.
+                    searchResults = range.search(longestWord, { matchCase: true, matchWholeWord: true });
+
+                    // Queue a commmand to load the font property of the results.
+                    context.load(searchResults, 'font');
+                })
+                .then(context.sync)
+                .then(function () {
+                    // Queue a command to highlight the search results.
+                    searchResults.items[0].font.highlightColor = '#FFFF00'; // Yellow
+                    searchResults.items[0].font.bold = true;
+                })
+                .then(context.sync);
+        })
+        .catch(errorHandler);
+    } 
+
+
+    function displaySelectedText() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    showNotification('The selected text is:', '"' + result.value + '"');
+                } else {
+                    showNotification('Error:', result.error.message);
+                }
+            });
+    }
+
+    //$$(Helper function for treating errors, $loc_script_taskpane_home_js_comment34$)$$
+    function errorHandler(error) {
+        // $$(Always be sure to catch any accumulated errors that bubble up from the Word.run execution., $loc_script_taskpane_home_js_comment35$)$$
+        showNotification("Error:", error);
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    }
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notification-header").text(header);
+        $("#notification-body").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+})();
